54ffc46697a67402ae3c04618fc6e73e2685b956
Sagnik Mondal <sagnik.mondal.20@gmail.com>
2024-03-20 06:46:23 -0400

    landing page

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import Highlights from './components/Highlights';
+import Hero from './components/Hero';
+
+const App = () => {
+
+  return (
+    <main className='bg-black'>
+      <Navbar />
+      <Hero />
+      <Highlights />
+    </main>
+  )
+}
+
+export default App
